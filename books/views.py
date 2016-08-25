6a4ffbc665715666fa9ea7a0da0d118f8c88735f
u68yj56res <rfwdasd23@interia.pl>
2016-08-25 20:21:13 +0200

    added book-wrapper

--- books/views.py
+++ books/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.views.generic import View
+from models import Book
 # Create your views here.
 
 # from django.contrib.admin.views.decorators import staff_member_required
@@ -9,3 +11,11 @@ from django.contrib.auth.decorators import login_required
 @login_required
 def dashboard(request):
 	return render(request,'index.html', {})
+	
+class BookView(View):
+	template_name = 'NAZWA.html'
+
+	def get(self, request, *args, **kwargs):
+		books = Book.objects.all()
+		return render(request, self.template_name, {'books':books})
+		#pass
---
+++ static/js/books.js
@@ -0,0 +1,4 @@
+$(document).ready(function() {
+    $('#book-wrapper').DataTable();
+} );
+
