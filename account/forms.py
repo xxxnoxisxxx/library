b5fdbf2945989b8381a7fd7238aebe173e3a3d41
xxxnoxisxxx <pawelnaj4@gmail.com>
2016-08-27 13:42:42 +0200

    Reader mobile field required RegisterReaderForm __init__ remove

--- account/forms.py
+++ account/forms.py
@@ -41,11 +41,6 @@ class RegisterUserForm(forms.ModelForm):
 
 
 class RegisterReaderForm(forms.ModelForm):
-    def __init__(self, *args, **kwargs):
-        super(RegisterReaderForm, self).__init__(*args, **kwargs)
-
-        self.fields['mobile'].required = True
-
     class Meta:
         model = Reader
         fields = ('id_card', 'mobile')
