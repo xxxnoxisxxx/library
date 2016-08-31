bcfbb0546cd3ae27c7950f4c50d855866e1fada0
Dawid Hujdus <hades13542@gmail.com>
2016-08-31 22:57:02 +0200

    Add book...

--- account/urls.py
+++ account/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url
 
-from account.views import LoginPageView, LogoutPageView, RegisterNewUserView, user_details, user_list
+from account.views import LoginPageView, LogoutPageView, RegisterNewUserView, user_details, user_list, AddNewBookView
 
 urlpatterns = [
     url(r'^login/', LoginPageView.as_view(), name='login'),
@@ -8,4 +8,5 @@ urlpatterns = [
     url(r'^register/', RegisterNewUserView.as_view(), name='register'),
     url(r'^user/$', user_list, name='user_list'),
     url(r'^user/(?P<username>[-\w]+)/$', user_details, name='user_detail'),
+    url(r'^add_book', AddNewBookView.as_view(), name='add_book'),
 ]
--- books/models.py
+++ books/models.py
@@ -5,79 +5,84 @@ from django.contrib.auth.models import User
 
 
 class Author(models.Model):
-	"""Class represents Author"""
+    """Class represents Author"""
 
-	name = models.CharField(max_length=35)
-	surname = models.CharField(max_length=35)
-		
-	class Meta:
-		db_table = 'author'
+    name = models.CharField(max_length=35)
+    surname = models.CharField(max_length=35)
 
-	def __unicode__(self):
-		return '%s %s' % (self.name, self.surname)
+    class Meta:
+        db_table = 'author'
+
+    def __unicode__(self):
+        return '%s %s' % (self.name, self.surname)
+
 
 class Publisher(models.Model):
-	"""Class represents publisher"""
+    """Class represents publisher"""
 
-	name = models.CharField(max_length=50)
+    name = models.CharField(max_length=50)
 
-	def __unicode__(self):
-		return self.name
+    def __unicode__(self):
+        return self.name
+
 
 class Book(models.Model):
-	"""Class represents book"""
+    """Class represents book"""
 
-	authors = models.ManyToManyField(Author, blank=True)
-	publisher = models.ForeignKey(Publisher, blank=True, null=True, on_delete=models.CASCADE)
+    authors = models.ManyToManyField(Author, blank=True)
+    publisher = models.ForeignKey(Publisher, blank=True, null=True, on_delete=models.CASCADE)
 
-	title = models.CharField(max_length=50)
-	isbn = models.CharField(max_length=20)
-	edition = models.IntegerField(blank=True, null=True)
-	edition_date = models.DateField(blank=True, null=True)
-	pages = models.IntegerField()
-	description = models.TextField()
-	
-	class Meta:
-		db_table = 'book'
+    title = models.CharField(max_length=50)
+    isbn = models.CharField(max_length=20)
+    edition = models.IntegerField(blank=True, null=True)
+    edition_date = models.DateField(blank=True, null=True)
+    pages = models.IntegerField()
+    description = models.TextField()
 
-	def __unicode__(self):
-		return self.title 
+    class Meta:
+        db_table = 'book'
+
+    def __unicode__(self):
+        return self.title
+
 
 class Item(models.Model):
-	"""Class represents book item"""
+    """Class represents book item"""
 
-	books = models.ForeignKey(Book, blank=True, null=True, on_delete=models.CASCADE)
-	available = models.BooleanField(default=True)
-	
-	class Meta:
-		db_table = 'item'
+    books = models.ForeignKey(Book, blank=True, null=True, on_delete=models.CASCADE)
+    available = models.BooleanField(default=True)
 
-	def __unicode__(self):
-		return self.books.title
+    class Meta:
+        db_table = 'item'
+
+    def __unicode__(self):
+        return self.books.title
+
 
 class Reservation(models.Model):
-	"""Class represents reservation"""
-	books = models.ForeignKey(Book, blank=True, null=True, on_delete=models.CASCADE)
-	
-	reservation_date = models.DateField()
-	reservation_status = models.IntegerField(default=7)
-	
-	class Meta:
-		db_table = 'reservation'
+    """Class represents reservation"""
+    books = models.ForeignKey(Book, blank=True, null=True, on_delete=models.CASCADE)
 
-	def __unicode__(self):
-		return "%s %s" %(self.id, self.books.title) 
-	
+    reservation_date = models.DateField()
+    reservation_status = models.IntegerField(default=7)
+
+    class Meta:
+        db_table = 'reservation'
+
+    def __unicode__(self):
+        return "%s %s" % (self.id, self.books.title)
+
+
 class Loan(models.Model):
-	"""Class represents lean book by reader"""
-	items = models.ForeignKey(Item, blank=True, null=True, on_delete=models.CASCADE)
+    """Class represents lean book by reader"""
+    items = models.ForeignKey(Item, blank=True, null=True, on_delete=models.CASCADE)
 
-	loan_date = models.DateField()
-	return_date = models.DateField()
-	loan_status = models.IntegerField()
-	
-	class Meta:
-		db_table = 'loan'
+    loan_date = models.DateField()
+    return_date = models.DateField()
+    loan_status = models.IntegerField()
 
-	def __unicode__(self):
-		return self.items.books.name + str(self.loan_date) + str(self.return_date)
+    class Meta:
+        db_table = 'loan'
+
+    def __unicode__(self):
+        return self.items.books.name + str(self.loan_date) + str(self.return_date)
